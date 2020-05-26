6a9fc8e728feb46a4709fa502cf22303515f9281
kodemannen <samuelknudsen@hotmail.com>
2020-05-26 22:31:43 +0200

    rewriting Plot_connectome()

--- main.py
+++ main.py
@@ -6,19 +6,27 @@ import seaborn as sns
 import sys
 # np.random.seed(eval(sys.argv[1]))
 np.random.seed(10)
+sns.set()
 
-def Plot_connectome(population, positions, connections, ax="none", save="no"):
 
-    if ax=="none":
-        sns.set()
-        fig, ax = plt.subplots()
+def Plot_connectome(population, positions, connections, ax):
 
     # Plotting connections:
-    n = len(connections)
-    for i in range(n):
+    pop_size = len(population)
+    pop_indices = np.arange(pop_size)
+    pop_gids = np.arange(population[0], population[-1]+1)
+
+    n_connections = len(connections)
+
+
+    for i in range(n_connections):
 
         synapse = connections[i] # Synapse number i in the network. (source-gid, target-gid, target-thread, synapse-id, port)
-        # positions[synapse[i][0]] is the position of the sender, positions[synapse[i][1]]
+        gid_sender = synapse[0]
+        gid_receiver = synapse[1]
+
+        index_sender = np.where()
+        index_receiver
 
         sender_position = positions[synapse[0]-1]
         receiver_position = positions[synapse[1]-1]
@@ -28,74 +36,78 @@ def Plot_connectome(population, positions, connections, ax="none", save="no"):
 
         ax.plot(xs,ys, linewidth=0.1, alpha=0.2, color="grey")
 
-    # plot neurons:
+    # Plotting neurons:
     ax.scatter(*zip(*positions), alpha=.7, color="grey")
 
-    #ax.axis("off")
-    if save=="yes":
-        fig.savefig("fig.pdf")
-    #plt.show()
+    return None
 
 
 
 def Create_population_positions(N, distribution):
 
-    if distribution=="uniform":
+    if distribution.lower()=="uniform":
         positions = np.random.uniform(low=0, high=1, size=(N, 2)) # the i'th list [j,k] means neuron i had position (j,k)
     elif distribution.lower()=="gaussian":
         positions = np.random.normal(loc=0, scale=1, size=(N, 2)) # the i'th list [j,k] means neuron i had position (j,k)
-    return 0
+    elif distribution.lower()=="column":
+        positions = np.zeros(shape=(N,2))
+        positions[:,1] = np.linspace(-1,1,N)
+
+    return positions
 
 
 
 if __name__ == "__main__":
 
+      ####################
+     # Hyperparameters: #
+    ####################
     simtime = 100.0 # ms
-    N = 100 # number of neurons
+    N_main = 100 # number of neurons
     N_sensory = 20
     plot = True
     epsilon = 0.1
-    indegree = 0
+    indegree = 10
     sensory_rate = 10.0 # Hz?
 
     nest.SetKernelStatus({"overwrite_files": True})
 
 
-
-
       ############################################
      # Creating network and experimental tools: #
     ############################################
-
     # Neuron populations:
-    main_population = nest.Create("iaf_psc_delta", N)
-    sensory_pop = nest.Create("parrot_neuron", N_sensory)
-    poisson_generator = nest.Create("poisson_generator")
+    population_main    = nest.Create("iaf_psc_delta", N_main)
+    population_sensory = nest.Create("parrot_neuron", N_sensory)
+    poisson_generator  = nest.Create("poisson_generator")
 
-    # Device needed for detecting the spikes of interest:
     spike_detector = nest.Create("spike_detector", params={"to_file":True,"label":"spike_times.txt"})
-
-
+    # Spike_detector is a device needed for gathering the spikes of the particular neurons we are interested in
 
 
 
       ###########################
      # Setting up connections: #
     ###########################
-    nest.Connect(sensory_pop, spike_detector)
-    nest.Connect(main_population, main_population, conn_spec={"rule": "fixed_indegree", "indegree": indegree})
-    nest.Connect(poisson_generator, sensory_pop)
+    nest.Connect(population_sensory, spike_detector)
+    nest.Connect(population_main, population_main, conn_spec={"rule": "fixed_indegree", "indegree": indegree})
+    nest.Connect(poisson_generator, population_sensory)
 
-    connections_main_pop = nest.GetConnections(target=main_population) # Element connections[i] is connection number i in the list of connections in the network? Can be given with an argument source=arg or target=arg. (source-gid, target-gid, target-thread, synapse-id, port)
+    # Fetching connectons for plotting:
+    connections_main    = nest.GetConnections(target=population_main)
+    connections_sensory = nest.GetConnections(target=population_sensory)
+    # connections_..[i] is connection number i in the list of connections in the network? Can be given with an argument source=arg or target=arg. (source-gid, target-gid, target-thread, synapse-id, port)
 
 
 
-     #######################
-    # Setting up positions: #
+      #########################
+     # Setting up positions: #
+    #########################
+    positions_main    = Create_population_positions(N_main, distribution="gaussian")
+    positions_sensory = Create_population_positions(N_sensory, distribution="column")
+
 
     nest.SetStatus(poisson_generator, {"rate": sensory_rate})
-
-
 
 
     nest.Simulate(simtime)
@@ -107,5 +119,9 @@ if __name__ == "__main__":
     #plt.show()
 
     if plot==True:
-        Plot_connectome(main_population, positions, connections_main_pop, save="yes")
+        fig, ax = plt.subplots()
+        Plot_connectome(population_main, positions_main, connections_main, ax)
+        print("hore")
+        Plot_connectome(population_sensory, positions_sensory, connections_sensory, ax)
+        fig.savefig("fig.pdf")
 
