e55d0e3ed84e58105bdf704c8e4007334a3a1e9b
kodemannen <samuelknudsen@hotmail.com>
2020-11-07 23:59:18 +0100

    Split up pixels

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import time
 import sys
 import os
+import seaborn as sns
+sns.set()
 
 from game.game import JumpGame
 from snn.snn import SNN
@@ -32,19 +34,51 @@ def main():
 
         game.play_one_step()
         pixels = game.get_pixels()  # input for the snn
-        print(pixels.shape) 
+        pixels = pixels.T.astype(np.float)
 
         # use just a cutout from the pixels as input, to simplify
-        input_ = pixels
+        #input_ = pixels
+
+        #print(input_1.shape) 
 
 
-        
+        pixels[:,:] /= 10053375
 
-        fig, ax = plt.subplots()
-        ax.imshow(pixels.T)
+        #pixels
+        #pixels[0:52, 400:500,] = 1 #np.random.randn(50, 100)
 
-        plt.savefig('output/game_testfig.png')
-        exit('jau')
+
+        square_side_x = 100           # n pixels per square that represents a retinal neuron 
+        square_size_y = 50
+        nx = 10
+        ny = 2
+
+        splitted = split_pixels(pixels)
+        print(splitted.shape)
+        exit('o')
+
+        #inputs = [np.arange(i,i+step)]
+
+        ## two lanes for now
+        ## one eye per lane
+        #for el in pixels.reshape(-1):
+        #    if el != 0:
+        #        print(el)
+        #        print('ballll')
+
+        #exit('i')
+
+        #
+        #img = np.ones(shape=(50,50)) * 0.3
+        ##img = np.random.randn(40, 40)
+
+        #fig, ax = plt.subplots()
+        #ax.imshow(pixels, )
+        ##ax.imshow(img,) 
+
+        #plt.savefig('output/game_testfig.png')
+        #exit('jau')
+        #print()
 
         # Create spikes out of the pixels?
         # a non zero pixel is a spike
@@ -53,6 +87,38 @@ def main():
 
 
 
+def split_pixels(img):
+    '''
+    Divide the pixels into compartments that are used as input to single neurons
+    '''
+
+    img_length = img.shape[1]   # length is n columns
+    img_height = img.shape[0]   # height is n rows
+
+    nx = 100
+    ny = 50
+
+    splitted = []
+
+    i = 0
+    j = 0
+    done = False
+    while not done:
+
+        cutout = img[j:j+ny, i:i+nx] 
+        splitted.append(cutout) 
+
+        # Move to next cutout:
+        i += nx
+        if i >= img_length - 1:
+            i = 0
+            j += ny
+
+        # Check if done:
+        if j >= img_height:
+            done = True
+        
+    return np.array(splitted)
 
 
 
